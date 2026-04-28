11333dec1fcb244aa2bd26d322aa8c0be0e470c6
kiieeaa <kiyaadinda5@gmail.com>
2026-04-28 11:51:21 +0700

    Mencari posisi yang tepat (ascending) untuk menyisipkan data di tengah atau di akhir

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -45,3 +45,14 @@ public:
             START = newNode;
             return;
         }
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll numbers not allowed" << endl;
+            return;
+        }
