625f8093d0d9db45de5393814b855343d1c7aa72
kiieeaa <kiyaadinda5@gmail.com>
2026-04-28 11:52:29 +0700

    Jika bukan di posisi terakhir, perbarui pointer prev dari node selanjutnya

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -58,3 +58,24 @@ public:
         }
         newNode->next = current->next;
         newNode->prev = current;
+
+        if (current->next != NULL)
+            current->next->prev = newNode;
+
+        current->next = newNode;
+    }
+
+    void hapus()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+        int rollNo;
+        cin >> rollNo;
+
+        Node *current = START;
+
